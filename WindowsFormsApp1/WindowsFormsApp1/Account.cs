990d4d4736f68ec80491e5aba9a53b06567b68cd
Mancitiss <vovuongthanhtu@gmail.com>
2022-05-09 10:58:20 +0700

    Added Client methods

---
+++ WindowsFormsApp1/WindowsFormsApp1/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Account
+    {
+        internal string username;
+        internal string name;
+        internal string id;
+
+        internal Account() { }
+        internal Account(string username, string name, string id)
+        {
+            this.username = username;
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/WeddingClient.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static WindowsFormsApp1.Tools;
+
+namespace WindowsFormsApp1
+{
+    internal class WeddingClient
+    {
+        private static string instruction;
+
+        internal static TcpClient client;
+        internal static SslStream stream;
+        internal static Account user;
+
+        private static int workeradded = 0;
+
+        internal static ConcurrentQueue<byte[]> commands = new ConcurrentQueue<byte[]>();
+
+        internal static void Ping()
+        {
+            try
+            {
+                string server_address = ConfigurationManager.AppSettings.Get("sever_address");
+                using (var client_2 = new TcpClient(server_address, Convert.ToInt16(ConfigurationManager.AppSettings.Get("port"))))
+                {
+                    using (var stream_2 = new SslStream(
+                        client_2.GetStream(),
+                        false,
+                        new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                        null
+                        ))
+                    {
+                        try
+                        {
+                            stream_2.AuthenticateAsClient(server_address);
+                        }
+                        catch (AuthenticationException e)
+                        {
+                            Console.WriteLine("Exception: {0}", e.Message);
+                            if (e.InnerException != null)
+                            {
+                                Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                            }
+                            Console.WriteLine("Authentication failed - closing the connection.");
+                            stream_2.Close();
+                            client_2.Close();
+                        }
+                        stream_2.Write(Combine(Encoding.Unicode.GetBytes("0012"), Encoding.ASCII.GetBytes(user.id)));
+                        Console.WriteLine("Pinged");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Logout()
+        {
+            //Ping();
+            user = null;
+            commands = new ConcurrentQueue<byte[]>();
+            stream.Dispose();
+            client.Dispose();
+            GC.Collect();
+        }
+
+        internal static void Queue_command(byte[] command)
+        {
+            commands.Enqueue(command);
+            Ping();
+            Add_worker_thread();
+        }
+
+        private static void Add_worker_thread()
+        {
+            Console.WriteLine("worker: {0}", workeradded);
+            if (0 == Interlocked.Exchange(ref workeradded, 1))
+            {
+                try
+                {
+                    ThreadPool.QueueUserWorkItem(Send_commands);
+                }
+                catch (NotSupportedException nse)
+                {
+                    Console.WriteLine(nse.ToString());
+                    try
+                    {
+                        Interlocked.Exchange(ref workeradded, 0);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private static void Send_commands(object state)
+        {
+            Console.WriteLine("start sending command");
+            try
+            {
+                while (!commands.IsEmpty)
+                    if (commands.TryDequeue(out byte[] command))
+                    {
+                        Console.WriteLine("Sending command...");
+                        stream.Write(command);
+                        Console.WriteLine("Command sent");
+                        Ping();
+                    }
+                Ping();
+                Interlocked.Exchange(ref workeradded, 0);
+                Console.WriteLine("Worker reset!");
+            }
+            catch (Exception se)
+            {
+                Console.WriteLine(se.ToString());
+                //throw se;
+            }
+        }
+
+        public static bool ValidateServerCertificate(
+             object sender,
+             X509Certificate certificate,
+             X509Chain chain,
+             SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }        
+    }
+}
